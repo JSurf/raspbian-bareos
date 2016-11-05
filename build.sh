582b9178bf501d82d821031d2cb942fdb84e3d90
JSurf <jsurf@gmx.de>
2016-11-05 18:48:25 +0100

    Install wget

--- build.sh
+++ build.sh
@@ -8,15 +8,16 @@ DIST=Debian_8.0
 # DIST=xUbuntu_12.04 
 URL=http://download.bareos.org/bareos/release/latest/$DIST/ 
  
+apt-get update
+apt-get install wget
+
 # add the Bareos repository 
 printf "deb-src $URL /\n" > /etc/apt/sources.list.d/bareos-src.list 
-
-apt-get install wget
 # add package key 
 wget -q $URL/Release.key -O- | apt-key add - 
 #mkdir -p /var/src/debian/; cd /var/src/debian  
-ls -l
 apt-get update
+
 apt-get source bareos
 #apt-get build-dep bareos
 apt-get install build-essential:native acl-dev autotools-dev bc chrpath debhelper git-core libacl1-dev libcap-dev libjansson-dev liblzo2-dev libqt4-dev libreadline-dev libssl-dev libwrap0-dev libx11-dev libsqlite3-dev libmysqlclient-dev libpq-dev logrotate lsb-release mtx ncurses-dev pkg-config po-debconf python-dev zlib1g-dev devscripts
