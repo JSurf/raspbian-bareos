5a60c01933f993a7a511bfc240126b2f989035de
Jens Viebig <jsurf@gmx.de>
2016-11-05 19:48:49 +0100

    use --build flag

--- build.sh
+++ build.sh
@@ -7,9 +7,6 @@ DIST=Debian_8.0
 # DIST=xUbuntu_14.04 
 # DIST=xUbuntu_12.04 
 URL=http://download.bareos.org/bareos/release/latest/$DIST/ 
- 
-apt-get update
-apt-get install wget
 
 # add the Bareos repository 
 printf "deb-src $URL /\n" > /etc/apt/sources.list.d/bareos-src.list 
@@ -18,9 +15,9 @@ wget -q $URL/Release.key -O- | apt-key add -
 #mkdir -p /var/src/debian/; cd /var/src/debian  
 apt-get update
 
-apt-get source bareos
 #apt-get build-dep bareos
 apt-get install acl-dev autotools-dev bc chrpath git-core libacl1-dev libcap-dev libjansson-dev liblzo2-dev libqt4-dev libreadline-dev libssl-dev libwrap0-dev libx11-dev libsqlite3-dev libmysqlclient-dev libpq-dev logrotate lsb-release mtx ncurses-dev pkg-config po-debconf python-dev zlib1g-dev
-
-cd bareos_15.2.2-37.1
-debuild -b -uc -us
+apt-get --build source bareos
+#ls -l
+#cd bareos_15.2.2-37.1
+#debuild -b -uc -us
